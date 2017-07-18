b190959a9ba632604df40586bc3b10308da08d93
sayanolee <sayano.lee@gmail.com>
2017-07-18 11:21:32 +0800

    modified

--- src/start2.py
+++ src/start2.py
@@ -3,7 +3,7 @@ import network2
 training_data, validation_data, test_data = \
 mnist_loader.load_data_wrapper()
 net = network2.Network([784,60,20,10])
-net.SGD(training_data, 30, 10, 1.25,lmbda=1000,\
+net.SGD(training_data, 30, 10, 1.25,lmbda=100,\
 		evaluation_data = validation_data,
 		monitor_evaluation_accuracy=True,
 		monitor_training_accuracy=True)
